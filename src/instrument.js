c0836a70d8ec2cf8952aaccdd2308c3b6eb2a63d
Abdellah Hariti <haritiabdellah@gmail.com>
2025-03-17 23:20:19 +0000

    use palceholder DSN (oops)

--- src/instrument.js
+++ src/instrument.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://644eb1de4da7067f595da41e3bb57634@o4506812586590208.ingest.us.sentry.io/4507300498702336",
+  dsn: "<your_DSN_key>",
   integrations: [],
   // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
   tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
